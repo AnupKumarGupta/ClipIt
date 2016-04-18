e2184a31d5a2bd4a33b3c7983a2029d7a3a1aaa7
GuptaAnupKumar <reachanupgupta@gmail.com>
2016-04-18 23:01:34 +0530

    Copy Paste Win Messages Started

--- ClipIt/ClipIt/ClipBoardHome.cs
+++ ClipIt/ClipIt/ClipBoardHome.cs
@@ -142,83 +142,108 @@ namespace ClipIt
             } 
             #endregion
         }
+
+        //public event EventHandler<ClipboardEventArgs> Pasted;
         protected override void WndProc(ref Message m)
         {
-            switch ((Win32.Msgs)m.Msg)
+            #region Commented
+            //#region switch
+            //var mx = m.Msg;
+            //switch ((Win32.Msgs)m.Msg)
+            //{
+            //    //
+            //    // The WM_DRAWCLIPBOARD message is sent to the first window 
+            //    // in the clipboard viewer chain when the content of the 
+            //    // clipboard changes. This enables a clipboard viewer 
+            //    // window to display the new content of the clipboard. 
+            //    //
+
+            //    case Win32.Msgs.WM_MOUSEMOVE:
+            //        ctlClipboardText.Text = "KeyDown";
+            //        break;
+
+            //    case Win32.Msgs.WM_DRAWCLIPBOARD:
+
+            //        Debug.WriteLine("WindowProc DRAWCLIPBOARD: " + m.Msg, "WndProc");
+
+            //        GetClipboardData();
+
+            //        //
+            //        // Each window that receives the WM_DRAWCLIPBOARD message 
+            //        // must call the SendMessage function to pass the message 
+            //        // on to the next window in the clipboard viewer chain.
+            //        //
+            //        Win32.User32.SendMessage(_ClipboardViewerNext, m.Msg, m.WParam, m.LParam);
+            //        break;
+
+
+            //    //
+            //    // The WM_CHANGECBCHAIN message is sent to the first window 
+            //    // in the clipboard viewer chain when a window is being 
+            //    // removed from the chain. 
+            //    //
+            //    case Win32.Msgs.WM_CHANGECBCHAIN:
+            //        Debug.WriteLine("WM_CHANGECBCHAIN: lParam: " + m.LParam, "WndProc");
+
+            //        // When a clipboard viewer window receives the WM_CHANGECBCHAIN message, 
+            //        // it should call the SendMessage function to pass the message to the 
+            //        // next window in the chain, unless the next window is the window 
+            //        // being removed. In this case, the clipboard viewer should save 
+            //        // the handle specified by the lParam parameter as the next window in the chain. 
+
+            //        //
+            //        // wParam is the Handle to the window being removed from 
+            //        // the clipboard viewer chain 
+            //        // lParam is the Handle to the next window in the chain 
+            //        // following the window being removed. 
+            //        if (m.WParam == _ClipboardViewerNext)
+            //        {
+            //            //
+            //            // If wParam is the next clipboard viewer then it
+            //            // is being removed so update pointer to the next
+            //            // window in the clipboard chain
+            //            //
+            //            _ClipboardViewerNext = m.LParam;
+            //        }
+            //        else
+            //        {
+            //            Win32.User32.SendMessage(_ClipboardViewerNext, m.Msg, m.WParam, m.LParam);
+            //        }
+            //        break;
+
+            //    case Win32.Msgs.WM_PASTE:
+            //        ctlClipboardText.Text = "Pasted";
+            //        break;
+
+            //    default:
+            //        //
+            //        // Let the form process the messages that we are
+            //        // not interested in
+            //        //
+            //        base.WndProc(ref m);
+            //        break;
+
+            //}
+            //#endregion
+            //if ((Win32.Msgs)m.Msg == Win32.Msgs.WM_COPY)
+            //{
+            //    GetClipboardData();
+
+            //} 
+            #endregion
+
+            Process proc = Process.GetCurrentProcess();
+          //  Win32.User32.SendMessage(proc.MainWindowHandle, (UInt32)WM_PASTE, (IntPtr)0, (IntPtr)0);
+
+            if ((Win32.Msgs)m.Msg == Win32.Msgs.WM_HOTKEY)
             {
-                //
-                // The WM_DRAWCLIPBOARD message is sent to the first window 
-                // in the clipboard viewer chain when the content of the 
-                // clipboard changes. This enables a clipboard viewer 
-                // window to display the new content of the clipboard. 
-                //
-                case Win32.Msgs.WM_DRAWCLIPBOARD:
-
-                    Debug.WriteLine("WindowProc DRAWCLIPBOARD: " + m.Msg, "WndProc");
-
-                    GetClipboardData();
-
-                    //
-                    // Each window that receives the WM_DRAWCLIPBOARD message 
-                    // must call the SendMessage function to pass the message 
-                    // on to the next window in the clipboard viewer chain.
-                    //
-                    Win32.User32.SendMessage(_ClipboardViewerNext, m.Msg, m.WParam, m.LParam);
-                    break;
-
-
-                //
-                // The WM_CHANGECBCHAIN message is sent to the first window 
-                // in the clipboard viewer chain when a window is being 
-                // removed from the chain. 
-                //
-                case Win32.Msgs.WM_CHANGECBCHAIN:
-                    Debug.WriteLine("WM_CHANGECBCHAIN: lParam: " + m.LParam, "WndProc");
-
-                    // When a clipboard viewer window receives the WM_CHANGECBCHAIN message, 
-                    // it should call the SendMessage function to pass the message to the 
-                    // next window in the chain, unless the next window is the window 
-                    // being removed. In this case, the clipboard viewer should save 
-                    // the handle specified by the lParam parameter as the next window in the chain. 
-
-                    //
-                    // wParam is the Handle to the window being removed from 
-                    // the clipboard viewer chain 
-                    // lParam is the Handle to the next window in the chain 
-                    // following the window being removed. 
-                    if (m.WParam == _ClipboardViewerNext)
-                    {
-                        //
-                        // If wParam is the next clipboard viewer then it
-                        // is being removed so update pointer to the next
-                        // window in the clipboard chain
-                        //
-                        _ClipboardViewerNext = m.LParam;
-                    }
-                    else
-                    {
-                        Win32.User32.SendMessage(_ClipboardViewerNext, m.Msg, m.WParam, m.LParam);
-                    }
-                    break;
-
-                case Win32.Msgs.WM_PASTE:
-                    ctlClipboardText.Text = "Pasted";
-                    break;
-
-                default:
-                    //
-                    // Let the form process the messages that we are
-                    // not interested in
-                    //
-                    base.WndProc(ref m);
-                    break;
-
+                ctlClipboardText.Text = "CUT";
             }
-
+            
+            base.WndProc(ref m);
         } 
 
         #endregion
-
 
         #region Internal Event Handlers
         private void frmMain_Load(object sender, System.EventArgs e)
